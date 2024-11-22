2d2af3653ed2e68d3cd0d8c9800ad9f07a101ae5
novitasalsabila <novnovv10@gmail.com>
2024-11-22 18:27:09 +0700

    Membuat fungsi utama aplikasi menggunakan jetpack compose

--- app/src/main/java/com/example/latihanucp2/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/latihanucp2/navigation/PengelolaHalaman.kt
@@ -25,3 +25,12 @@ enum class Halaman{
     Tampil
 }
 
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+
+}
