8edffde8578c29b51a7eda26ee7d42829cd94111
novitasalsabila <novnovv10@gmail.com>
2024-11-22 17:53:38 +0700

    Deklarasi sebuah object di Kotlin, digunakan sebagai singleton (hanya ada satu instance di seluruh aplikasi).

--- app/src/main/java/com/example/latihanucp2/data/MataKuliah.kt
+++ app/src/main/java/com/example/latihanucp2/data/MataKuliah.kt
@@ -1,12 +1,5 @@
 package com.example.latihanucp2.data
 
 object MataKuliah {
-    val options = listOf(
-        "Pemograman Aplikasi Mobile Lanjut",
-        "Pengembangan Aplikasi Web Framework",
-        "Prototyping",
-        "Aplikasi Multimedia",
-        "Kecerdasan Buatan",
-        "Computer Vission"
-    )
+
 }
