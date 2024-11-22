3a836d14a8818eed987999a1b140c4bddab76ff1
novitasalsabila <novnovv10@gmail.com>
2024-11-22 19:51:05 +0700

    class`MahasiswaViewModel` bertugas untuk menyimpan dan mengelola data mahasiswa agar tetap tersedia meskipun aplikasi diubah atau layar diputar, serta memberikan akses data tersebut ke tampilan UI.

--- app/src/main/java/com/example/latihanucp2/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/latihanucp2/ui/viewmodel/MahasiswaViewModel.kt
@@ -8,16 +8,17 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel :ViewModel() {
-    //Request
+    // Request: Variabel untuk menyimpan status data mahasiswa yang dapat berubah
     private val _statusUI = MutableStateFlow(Mahasiswa())
 
-    //Response
+    // Response: Mengubah _statusUI menjadi StateFlow yang hanya bisa dibaca (immutable)
     val statusUI: StateFlow<Mahasiswa> = _statusUI.asStateFlow()
-
+    // Fungsi untuk menyimpan data mahasiswa ke dalam _statusUI
     fun saveDataSiswa(ls: MutableList<String>){
+        // Update nilai _statusUI dengan data yang baru
         _statusUI.update { statusSaatIni -> //isi sebuah model: datasiswa
             statusSaatIni.copy(
-                nim = ls[0],
+                nim = ls[0], // Menyimpan NIM dari list
                 nama = ls[1],
                 email = ls[2],
 
