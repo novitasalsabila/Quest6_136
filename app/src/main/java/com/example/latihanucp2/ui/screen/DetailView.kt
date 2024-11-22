9cdfd5fc6b8d4b50e666ceda6501ed9f047f1c06
novitasalsabila <novnovv10@gmail.com>
2024-11-22 19:08:15 +0700

    Membuat fungsi detail view dan detai row untuk menampilkan label dan nilai data dalam format berbaris

--- app/src/main/java/com/example/latihanucp2/ui/screen/DetailView.kt
+++ app/src/main/java/com/example/latihanucp2/ui/screen/DetailView.kt
@@ -18,67 +18,16 @@ import com.example.latihanucp2.model.Mahasiswa
 import com.example.latihanucp2.model.RencanaStudi
 
 @Composable
-fun DatailView(
+fun DetailView(
     mahasiswa: Mahasiswa,
     rencanaStudi: RencanaStudi,
     onBackButtonClicked: () -> Unit,
     onResetButtonClicked: () -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        Text("Detail Mahasiswa", fontSize = 20.sp, fontWeight = FontWeight.Bold)
 
-        // Menampilkan data Mahasiswa
-        DetailRow(label = "Nama", value = mahasiswa.nama)
-        DetailRow(label = "NIM", value = mahasiswa.nim)
-        DetailRow(label = "Email", value = mahasiswa.email)
-
-        Spacer(modifier = Modifier.padding(8.dp))
-
-        // Menampilkan Rencana Studi
-        Text("Rencana Studi", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-        DetailRow(label = "Mata Kuliah", value = rencanaStudi.mataKuliah)
-        DetailRow(label = "Kelas", value = rencanaStudi.kelas)
-
-        Spacer(modifier = Modifier.padding(8.dp))
-
-        // Tombol Navigasi
-        Button(
-            onClick = onBackButtonClicked,
-            modifier = Modifier.fillMaxWidth(),
-            interactionSource = remember { MutableInteractionSource() } // Tambahkan ini
-        ) {
-            Text("Kembali")
-        }
-
-
-        Spacer(modifier = Modifier.padding(4.dp)) // Menambahkan jarak antar tombol
-
-        Button(
-            onClick = onResetButtonClicked,
-            modifier = Modifier.fillMaxWidth(),
-            interactionSource = remember { MutableInteractionSource() } // Tambahkan ini
-        ) {
-            Text("Reset")
-        }
-    }
 }
 
 @Composable
 fun DetailRow(label: String, value: String) {
-    Column(modifier = Modifier.padding(bottom = 8.dp)) {
-        Text(
-            text = label,
-            fontWeight = FontWeight.Bold,
-            fontSize = 16.sp
-        )
-        Text(
-            text = value,
-            fontSize = 14.sp
-        )
-    }
+
 }
