3429e6cf45690493b57800bbc8fc8680494e8b65
novitasalsabila <novnovv10@gmail.com>
2024-11-22 19:39:16 +0700

    Membuat column untuk menampilkan label data

--- app/src/main/java/com/example/latihanucp2/ui/screen/DetailView.kt
+++ app/src/main/java/com/example/latihanucp2/ui/screen/DetailView.kt
@@ -74,5 +74,15 @@ fun DetailView(
 
 @Composable
 fun DetailRow(label: String, value: String) {
-
+    Column(modifier = Modifier.padding(bottom = 8.dp)) {
+        Text(
+            text = label,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp
+        )
+        Text(
+            text = value,
+            fontSize = 14.sp
+        )
+    }
 }
