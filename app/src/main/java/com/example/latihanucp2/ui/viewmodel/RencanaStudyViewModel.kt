c4be0475fca517550781551f53fe6f8eb72121ae
novitasalsabila <novnovv10@gmail.com>
2024-11-22 19:54:56 +0700

    RencanaStudyViewModel mengelola data rencana studi dan memberikan cara untuk memperbarui serta mengaksesnya dalam UI.

--- app/src/main/java/com/example/latihanucp2/ui/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/latihanucp2/ui/viewmodel/RencanaStudyViewModel.kt
@@ -8,17 +8,22 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel() {
+    // Menyimpan state rencana studi yang dapat berubah (mata kuliah dan kelas)
     private val _krsState = MutableStateFlow(RencanaStudi())
+    // Menyediakan versi hanya-baca dari _krsState untuk diobservasi oleh UI
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 
+    // Fungsi untuk mengatur mata kuliah pada rencana studi
     fun setMatakuliah(mkPilihan: String) {
+        // Memperbarui state rencana studi dengan mata kuliah yang baru
         _krsState.update {  stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
     }
-
+    // Fungsi untuk mengatur kelas pada rencana studi
     fun setKelas(kelasPilihan: String) {
         _krsState.update {  stateMK -> stateMK.copy(kelas = kelasPilihan) }
     }
 
+    //Fungsi untuk menyimpan data mata kuliah dan kelas dari list ke dalam rencana studi
     fun saveDataKRS(ls: MutableList<String>){
         _krsState.update { status -> status.copy(
             mataKuliah = ls[0],
